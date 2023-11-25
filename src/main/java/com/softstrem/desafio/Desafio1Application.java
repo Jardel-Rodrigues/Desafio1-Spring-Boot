be1f343c2adec8c29cbdb9f32f2d3590e182cedd
Jardel Rodrigues <rodriguinhosilvards96@gmail.com>
2023-11-24 22:30:15 -0300

    Implementado o projeto Obs. ainda falta arruma a logica de calculo do valor tatol do produto.

--- src/main/java/com/softstrem/desafio/Desafio1Application.java
+++ src/main/java/com/softstrem/desafio/Desafio1Application.java
@@ -1,12 +1,21 @@
 package com.softstrem.desafio;
 
+import java.util.Locale;
+import java.util.Scanner;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.softstrem.desafio.entities.Order;
+import com.softstrem.desafio.services.OrderService;
+
 @SpringBootApplication
 public class Desafio1Application implements CommandLineRunner {
 	
+	@Autowired
+	private OrderService orderService;
 
 	public static void main(String[] args) {
 		SpringApplication.run(Desafio1Application.class, args);
@@ -14,9 +23,21 @@ public class Desafio1Application implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Teste");
+		Locale.setDefault(Locale.US);
+		Scanner input = new Scanner(System.in);
 		
+		int code = input.nextInt();
+		double basic = input.nextDouble();
+		double discount = input.nextDouble();
 		
+		Order order = new Order(code, basic, discount);
+		
+		double total = orderService.total(order);
+		
+		System.out.println("Pedido código " + code);
+		System.out.println(String.format("Valor total: R$ %.2f", total));
+		
+input.close();		
 	}
 
 }
